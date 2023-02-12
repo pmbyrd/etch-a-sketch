6a5f3c0f628e9ea8cea90284a053170830b2ff6f
Perriesha <pmbyrd@gmail.com>
2023-02-11 23:24:00 -0800

    Added prompt

--- app.js
+++ app.js
@@ -1,33 +1,45 @@
 console.log("hello world");
-$("button").on("click", function() {
+$("button").on("click", function () {
   console.log("button clicked");
+//   let x = prompt("How many squares wide?");
+// turn the string into a number
+
 });
 
 // create a grix where x length is equal to y length
-let x = 16;
-let y = 16;
+let x;
+let y;
 let grid = [];
-let $EAS = $(".etch-a-sketch")
+let $EAS = $(".etch-a-sketch");
 
-function makeGrid(x,y) {
-    for (let i = 0; i < x; i++) {
-        grid.push([]);
-        for (let j = 0; j < y; j++) {
-        grid[i].push(0);
-        }
+function makeGrid(x, y) {
+  for (let i = 0; i < x; i++) {
+    grid.push([]);
+    for (let j = 0; j < y; j++) {
+      grid[i].push(0);
     }
+  }
 }
 
 function createSquare() {
-    let $square = $("<div>").addClass("square").css("width", 100/x + "%").css("height", 100/y + "%");
-    $EAS.append($square);
-    return $square;
+  let $square = $("<div>")
+    .addClass("square")
+    .css("width", 100 / x + "%")
+    .css("height", 100 / y + "%");
+  $EAS.append($square);
+  return $square;
 }
 
 function displayGrid() {
-    for (let i = 0; i < x; i++) {
-        for (let j = 0; j < y; j++) {
-        createSquare();
-        }
+  for (let i = 0; i < x; i++) {
+    for (let j = 0; j < y; j++) {
+      createSquare();
     }
+  }
 }
+
+// Todo get user input
+// Todo create a grid based on user input
+// Todo add event listener to each square
+// *first try with the hover
+// Todo change color of square when clicked
